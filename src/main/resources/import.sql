a5e20d42e51d627c55996b950f3964d0806fe257
sotowang <sotowang@qq.com>
2019-05-06 15:01:15 +0800

    sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,4 +5,4 @@ insert into identity (id,role) values (1,'普通用户');
 insert into department (id,name) values (1,'未知部门');
 
 INSERT into user(cardid, name, password, permission, sex, status, identity, department)
-values ('admin','管理员','123456',1,1,1,1,1);
+values ('admin','管理员','admin',1,1,1,1,1);
